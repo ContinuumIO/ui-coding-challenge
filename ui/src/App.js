91e41d37b45d625e913e07753e89eb0a621973a9
andrewmoon1 <andrew.moon1@yahoo.com>
2017-03-06 08:32:42 -0600

    added React to Webpack

---
+++ ui/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  componentDidMount() {
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">
+          Hello, World
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
--- ui/src/main.js
+++ ui/src/main.js
@@ -1,8 +1,16 @@
-import './index.scss';
+// import './index.scss';
+//
+// import { alertHello } from './hello';
+//
+// document.addEventListener('DOMContentLoaded', function(event) {
+//   let name = window.prompt('Who would you like to greet?');
+//   alertHello(name);
+// });
 
-import { alertHello } from './hello';
-
-document.addEventListener('DOMContentLoaded', function(event) {
-  let name = window.prompt('Who would you like to greet?');
-  alertHello(name);
-});
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
